8bfb3d5cfd128acc6fc3efd12e8f11032feb1752
ayoubben18 <ayoubbensalah2004@gmail.com>
2024-01-29 18:50:17 +0100

    adding the auctions service

---
+++ src/AuctionService/Entities/Item.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AuctionService.Entities;
+
+[Table("Items")]
+public class Item
+{
+    public Guid Id { get; set; }
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public string Color { get; set; }
+    public int Mileage { get; set; }
+    public string ImageUrl { get; set; }
+
+    //nav props
+    public Auction Auction { get; set; }
+    public Guid AuctionId { get; set; }
+}
---
+++ src/AuctionService/Program.cs
@@ -0,0 +1,34 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AuctionDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+var app = builder.Build();
+
+//middlware
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+    DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+
+app.Run();
